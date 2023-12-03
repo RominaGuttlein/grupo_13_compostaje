8ea9ac50571fc9f6f517c9e54980fbe544c68490
g-j-cabral <gwen28j@gmail.com>
2023-12-03 20:16:18 -0300

    javascript en formulario con sweetAlert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,40 @@
+/*
+function confirmarEnvio() {
+  return confirm("¿Estás seguro de que deseas enviar el formulario?");
+} */
 
-/*no veo que ande el cancelar el envio del formulario
-  function limpiarFormulario() {   
-    var formulario = document.getElementById("formulario");
-    formulario.reset();
-  }
-  */
+function confirmarEnvio() {
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: '¿Deseas enviar el formulario?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonText: 'Sí, enviar',
+    cancelButtonText: 'No, cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {      
+      document.getElementById('miFormulario').submit(); 
+    }
+  });
+
+  return false;
+    }
+
+
+function confirmarCancelacion() {
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: 'Si cancelas, la información no se enviará.',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Sí, cancelar',
+    cancelButtonText: 'No, continuar'
+  }).then((result) => {
+    if (result.isConfirmed) {  
+      Swal.fire('Cancelado', 'El envío ha sido cancelado', 'info');
+    }
+  });
+
+  return false;  
+}
+
