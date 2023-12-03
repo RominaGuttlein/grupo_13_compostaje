a4f3ea9a07041e07dbd0022e5153d5f079b81bf0
g-j-cabral <gwen28j@gmail.com>
2023-12-03 17:35:28 -0300

    agregue video y mas informacion al index

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+/*no veo que ande el cancelar el envio del formulario
+  function limpiarFormulario() {   
+    var formulario = document.getElementById("formulario");
+    formulario.reset();
+  }
+  */
